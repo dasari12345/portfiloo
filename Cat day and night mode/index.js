0adf9456b364932e5ae265bf78c1317a676ad158
dasari12345 <147372041+dasari12345@users.noreply.github.com>
2023-10-09 19:17:39 +0530

    Update index.js

--- Cat day and night mode/index.js
+++ Cat day and night mode/index.js
@@ -1,1 +1,12 @@
-
+const cat = document.getElementById("cat");
+const btn = document.getElementById("btn");
+btn.addEventListener("click", Cat);
+function Cat() {
+    if (btn.textContent == "Click Me") {
+        cat.src = "cateyes.jpeg";
+        btn.textContent = "Close Me";
+    } else {
+        cat.src = "catfull.jpeg";
+        btn.textContent = "Click Me";
+    }
+}
