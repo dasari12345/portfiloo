f73ace6cc5d19a3cc4639d0cb136a8d03061252d
dasari12345 <147372041+dasari12345@users.noreply.github.com>
2023-10-09 19:13:55 +0530

    Update index.js

--- Bulb on and off/index.js
+++ Bulb on and off/index.js
@@ -1,1 +1,12 @@
-
+const bulb = document.getElementById("bulb");
+const btn = document.getElementById("btn");
+btn.addEventListener("click", Bulb);
+function Bulb(){
+    if(btn.textContent === "ON"){
+        bulb.src ="https://i.postimg.cc/6QyTynzr/bulb-on.png";
+        btn.textContent ="OFF";
+    }else {
+        bulb.src ="https://i.postimg.cc/KjK1wL3c/bulb-off.png";
+        btn.textContent = "ON";
+    }
+}
